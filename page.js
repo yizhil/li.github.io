fa36bdb52cfd3ef21a76a07e4cfc4c5d47d64ead
yizhili <yizli017@gmail.com>
2020-06-30 23:11:44 +0100

    x

--- page.js
+++ page.js
@@ -10,7 +10,7 @@ var data = {
             img: 'images/option.png',
 			authors: 'QFGB 8960 Advanced C++',
             title: 'Scalable PDE Solver for Options Pricing',
-            text: 'Design, build and implement a PDE solver using finite difference method. The solver aims to compute value and Greeks for Call/Put, Eur/Ame/Bermuda, Vanilla/Barrier options, a total of 24 type combinations. The solver takes great advantage of polymorphimsm and inheritance, thus can be easily expanded to numerically solve other PDEs or price other option with its payoff provided. [<a href="https://github.com/yizhil/OptionPricing">code</a>][<a href="https://github.com/yizhil/OptionPricing/tree/master/results">pics</a>]'
+            text: 'Design, build and implement a PDE solver using 3 finite difference methods (Explicit, Implicit, and Crank-Nicolson). The solver aims to compute value and Greeks for Call/Put, Eur/Ame/Bermuda, Vanilla/Barrier options, a total of 24 type combinations and compare 3 methods. The solver takes great advantage of polymorphimsm and inheritance, and thus can be easily expanded to numerically solve other PDEs or price other option with its payoff provided. [<a href="https://github.com/yizhil/OptionPricing">code</a>][<a href="https://github.com/yizhil/OptionPricing/tree/master/results">pics</a>]'
         },
         {
             img: 'images/trump.png',
